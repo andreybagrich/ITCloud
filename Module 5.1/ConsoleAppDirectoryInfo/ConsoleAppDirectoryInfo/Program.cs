0e2dba1a9444303bd69648743adc18c0147ae342
andrey <andrey@DESKTOP-C1QE1GB>
2019-01-05 15:36:04 +0200

    IoCContainer create

--- Module 5.1/ConsoleAppDirectoryInfo/ConsoleAppDirectoryInfo/Program.cs
+++ Module 5.1/ConsoleAppDirectoryInfo/ConsoleAppDirectoryInfo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace ConsoleAppDirectoryInfo
     {
         static void Main(string[] args)
         {
+            
+
+            DirectoryInfo directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            Directory.CreateDirectory(directory + "test");
+            string path = directory + @"test" + @"test.txt";
+
+            File.Create(path);
+
+            //using (Stream s = new FileStream("text.txt", FileMode.Create))
+            //{
+
+            //    string text = "Here be some text";
+            //    byte[] arrBytes = text.SelectMany(c => BitConverter.GetBytes(c)).ToArray(); //единый массив > select
+
+            //    //s.Write(arrBytes, 0, arrBytes.Length);
+            //    //s.Flush();
+
+            //    s.Write(arrBytes, 0, arrBytes.Length);
+
+            //    s.Position = 0;
+            //    s.Write(arrBytes, 0, arrBytes.Length);
+            //    s.Flush();
+            //}
+
+
         }
     }
 }
